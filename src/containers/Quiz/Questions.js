2431716d82e81f8ecacaf860dcb62bcf444682a9
James Garcia <james.garcia1201@outlook.com>
2022-07-29 11:13:05 -0500

    created question 4 getting Livscore from user

--- src/containers/Quiz/Questions.js
+++ src/containers/Quiz/Questions.js
@@ -3,6 +3,7 @@ import React, { useState, useCallback } from 'react';
 import Q1 from './Qs/Question1/Question1';
 import Q2 from './Qs/Question2/Question2';
 import Q3 from './Qs/Question3/Question3';
+import Q4 from './Qs/Question4/Question4';
 
 import './Questions.css';
 
@@ -31,6 +32,7 @@ const Questions = () => {
      const [luxScore, setLuxScore] = useState(0);
      const [email, setEmail] = useState('');
      const [hoods, setHoods] = useState([]);
+     const [inUnitWasherDry, setInUnitWasherDry] = useState(true);
 
      // Change Handlers:
      const handleNameChange = useCallback((newName) => {
@@ -45,11 +47,16 @@ const Questions = () => {
           setLuxScore(newScore);
      }, [])
 
+     const handleLivChange = useCallback((newScore) => {
+          setLivScore(newScore);
+     }, [])
+
      return (
           <div id='questionBox' className='container'>
                {question === 1 && <Q1 userName={name} nextQuestion={nextQuestion} handleNameChange={handleNameChange}/>}
                {question === 2 && <Q2 userName={name} previousQuestion={previousQuestion} nextQuestion={nextQuestion} updateHoods={handleHoodChange} hoods={hoods} />}
                {question === 3 && <Q3 luxScore={luxScore} previousQuestion={previousQuestion} nextQuestion={nextQuestion} updateLuxScore={handleLuxChange} />}
+               {question === 4 && <Q4 livScore={livScore} previousQuestion={previousQuestion} nextQuestion={nextQuestion} updateLivScore={handleLivChange} />}
           </div>
      )
 }
